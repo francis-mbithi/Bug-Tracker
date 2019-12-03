230390d613ac3e58391d5839432fc65e9611838b
Cyprian Bowen <bowencyprian@gmail.com>
2019-12-03 12:32:32 +0300

    add setter methods

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -21,4 +21,16 @@ public class User {
     public int getId() {
         return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
