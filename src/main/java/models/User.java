83c4564c87efcbae3c6e1f4915ff040f7a038b9b
Cyprian Bowen <bowencyprian@gmail.com>
2019-12-03 12:30:45 +0300

    inittialize the variables

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,5 +1,13 @@
 package models;
 
 public class User {
+    private String username;
+    private String email;
+    private int id;
+
+    public User(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
 
 }
