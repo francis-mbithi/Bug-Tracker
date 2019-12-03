fcd6cfe585825c64e9e4a1f5c27a99802ea7e46f
Cyprian Bowen <bowencyprian@gmail.com>
2019-12-03 14:46:30 +0300

    test for user getter and setter methods

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,7 +1,47 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
 public class UserTest {
+    @Before
+    public void setUp() throws Exception {
 
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    public User setupUser(){
+        return new User("Mike", "mike@mike.com" );
+    }
+    @Test
+    public void getUsernameReturnsCorrectly(){
+        User user = setupUser();
+        assertEquals("Mike", user.getUsername());
+    }
+    @Test
+    public void setUsernameReturnsCorrectly(){
+        User user = setupUser();
+        user.setUsername("Mike");
+        assertEquals("Mike", user.getUsername());
+    }
+    @Test
+    public void getEmailReturnsCorrectly(){
+        User user = setupUser();
+        assertEquals("mike@mike.com", user.getEmail());
+    }
+    @Test
+    public void setEmailReturnsCorrectly(){
+        User user =setupUser();
+        user.setEmail("mike@mike.com");
+        assertEquals("mike@mike.com", user.getEmail());
+    }
 }
